68ca024543247797aad4fd0b5dcc7ded80b49e52
vitorotto <vitorotto05@gmail.com>
2025-10-18 17:25:02 -0300

    Aula 04: native_splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,40 +27,8 @@ class AppHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: AppBar(
-        title: const Text(".appable/"),
-        leading: const Icon(Icons.ondemand_video),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add_shopping_cart_outlined),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-            Text("Heading", style: Theme.of(context).textTheme.headlineLarge),
-            Text("Sub-heading", style: Theme.of(context).textTheme.titleMedium),
-            Text("Paragraph", style: Theme.of(context).textTheme.labelLarge),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Elevated Button"),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Outlined Button"),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Image(
-                image: NetworkImage(
-                  "https://www.popsci.com/wp-content/uploads/2024/07/buy_physical_books.jpg",
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text("./appable"),),
+      body: Center(child: Text("Home page"),),
     );
   }
 }
